df7fc24e9e477d8e1cdfaea9f0df47fd3c7d26bf
Allan Taylor <allan@startmail.com>
2021-10-11 09:02:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,11 +68,10 @@ resource "azurerm_subnet" "gateway_subnet" {
 }
 
 resource "azurerm_local_network_gateway" "ptp_vpn_local_gw" {
-  count               = var.enable_ptp_vpn == true ? 1 : 0
   name                = var.ptp_vpn_remote_gw_name
   resource_group_name = azurerm_resource_group.network_rg.name
   location            = var.region
-  gateway_address     = var.ptp_vpn_remote_endpoint
+  gateway_fqdn        = var.ptp_vpn_remote_endpoint
   address_space       = [var.ptp_vpn_remote_network]
 
   tags = {
@@ -81,8 +80,7 @@ resource "azurerm_local_network_gateway" "ptp_vpn_local_gw" {
 }
 
 resource "azurerm_public_ip" "virtual_network_gateway_public_ip" {
-  count               = var.enable_ptp_vpn == true ? 1 : 0
-  name                = "${var.environment_tag}-${var.region}-PTP-VPN-IP"
+  name                = "${var.environment_tag}-${var.region}-VGW-IP"
   location            = var.region
   resource_group_name = azurerm_resource_group.network_rg.name
   allocation_method   = "Dynamic"
@@ -94,14 +92,13 @@ resource "azurerm_public_ip" "virtual_network_gateway_public_ip" {
 
 resource "azurerm_virtual_network_gateway" "virtual_network_gateway" {
   depends_on          = [azurerm_subnet_route_table_association.default_subnet_route_table_association]
-  count               = var.enable_ptp_vpn == true ? 1 : 0
-  name                = "${var.environment_tag}-${var.region}-PTP-VPN-GW"
+  name                = "${var.environment_tag}-${var.region}-VPN-GW"
   location            = var.region
   resource_group_name = azurerm_resource_group.network_rg.name
   type                = "Vpn"
   vpn_type            = "RouteBased"
   enable_bgp          = false
-  sku                 = var.virtual_network_gateway_sku
+  sku                 = "Basic"
 
   ip_configuration {
     name                          = "vnetGatewayConfig"
@@ -116,25 +113,24 @@ resource "azurerm_virtual_network_gateway" "virtual_network_gateway" {
 }
 
 resource "azurerm_virtual_network_gateway_connection" "onpremise" {
-  count                      = var.enable_ptp_vpn == true ? 1 : 0
   depends_on                 = [azurerm_virtual_network_gateway.virtual_network_gateway]
-  name                       = "${var.environment_tag}-${var.region}-PTP-VPN-GW-Connections"
+  name                       = "${var.environment_tag}-${var.region}-VPN-On-Prem"
   location                   = var.region
   resource_group_name        = azurerm_resource_group.network_rg.name
   type                       = "IPsec"
-  virtual_network_gateway_id = azurerm_virtual_network_gateway.virtual_network_gateway[count.index].id
-  local_network_gateway_id   = azurerm_local_network_gateway.ptp_vpn_local_gw[count.index].id
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.virtual_network_gateway.id
+  local_network_gateway_id   = azurerm_local_network_gateway.ptp_vpn_local_gw.id
   shared_key                 = var.ptp_vpn_psk
 
-  ipsec_policy {
-    ike_encryption   = var.ptp_vpn_ike_encryption
-    ike_integrity    = var.ptp_vpn_ike_integrity
-    dh_group         = var.ptp_vpn_dh_group
-    ipsec_encryption = var.ptp_vpn_ipsec_encryption
-    ipsec_integrity  = var.ptp_vpn_ipsec_integrity
-    pfs_group        = var.ptp_vpn_pfs_group
-    sa_lifetime      = var.ptp_vpn_sa_lifetime
-  }
+  #ipsec_policy {
+  #  ike_encryption   = var.ptp_vpn_ike_encryption
+  #  ike_integrity    = var.ptp_vpn_ike_integrity
+  #  dh_group         = var.ptp_vpn_dh_group
+  #  ipsec_encryption = var.ptp_vpn_ipsec_encryption
+  #  ipsec_integrity  = var.ptp_vpn_ipsec_integrity
+  #  pfs_group        = var.ptp_vpn_pfs_group
+  #  sa_lifetime      = var.ptp_vpn_sa_lifetime
+  #}
 
   tags = {
     environment = var.environment_tag
@@ -142,7 +138,6 @@ resource "azurerm_virtual_network_gateway_connection" "onpremise" {
 }
 
 resource "azurerm_route" "vpn_route" {
-  count               = var.enable_ptp_vpn == true ? 1 : 0
   depends_on          = [azurerm_virtual_network_gateway.virtual_network_gateway]
   name                = "VPNRoute"
   resource_group_name = azurerm_resource_group.network_rg.name
